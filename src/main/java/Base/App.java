4dcbbe5c3446cdebc03c5c9f84d70fa7eead35a7
Alek Dussuau <ajdussuau@gmail.com>
2021-06-06 21:38:26 -0400

    Initial Solution

--- src/main/java/Base/App.java
+++ src/main/java/Base/App.java
@@ -32,8 +32,37 @@ Implement this program as a GUI program that automatically updates the values wh
 Modify the program so it also supports the Kelvin scale.
  */
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args){
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Press C to convert from Fahrenheit to Celsius");
+        System.out.println("Press F to convert from Celsius to Fahrenheit.");
+        System.out.print("Your choice: ");
+        String transfer = input.nextLine();
+
+        if(transfer.equalsIgnoreCase("c")){
+            System.out.print("please enter the temperature in Fahrenheit: ");
+            String buff_temp = input.nextLine();
+
+            double temp = Double.parseDouble(buff_temp);
+
+            double new_temp = (temp-32)*5/9;
+
+            System.out.printf("The temperature in Celsius is %.2f°", new_temp);
+        }else if (transfer.equalsIgnoreCase("f")){
+            System.out.print("please enter the temperature in Celsius: ");
+            String buff_temp = input.nextLine();
+
+            double temp = Double.parseDouble(buff_temp);
+
+            double new_temp = (temp*9/5)+32;
+
+            System.out.printf("The temperature in Fahrenheit is %.2f°", new_temp);
+        }
+
+
     }
 }
